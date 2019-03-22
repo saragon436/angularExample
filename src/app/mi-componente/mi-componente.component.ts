6c75ebf6e9d24e6e85574e75fa9726290f018dc4
Sergio Aragon <saragon436@gmail.com>
2019-03-22 12:37:02 -0500

    se realizo la tarea 2 y esta comenzando la 3

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {iTarea} from "src/app/modelos/Tarea";
 
 @Component({
   selector: 'app-mi-componente',
@@ -7,28 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mi-componente.component.css']
 })
 export class MiComponenteComponent implements OnInit {
-  permitido: boolean = true
-  tareas: Array<string>=["tarea0","tarea1","tarea2"]
-  estado: string= "no se ha agregado ninguna tarea"
-  nombreTarea: string =""
-  tareaAgregada: boolean = false
+  // permitido: boolean = true
+  // tareas: Array<string>=["tarea0","tarea1","tarea2"]
+  // estado: string= "no se ha agregado ninguna tarea"
+  // nombreTarea: string =""
+  // tareaAgregada: boolean = false
+  tareas: Array<iTarea> = []
   constructor() {
-    setTimeout(() => {
-      this.permitido = false
-    }, 63000);
+    // setTimeout(() => {
+    //   this.permitido = false
+    // }, 63000);
+    this.tareas.push({titulo:"tarea 1",fechaRegistro:new Date()})
+    this.tareas.push({titulo:"tarea 2",fechaRegistro:new Date()})
+    this.tareas.push({titulo:"tarea 3",fechaRegistro:new Date()})
    }
 
-   agregarTarea(){
-    this.estado=`se agregado una tarea
-     ${this.nombreTarea}
-    `
-    this.tareaAgregada=true
-    this.tareas.push(this.nombreTarea)
-   }
+  //  agregarTarea(){
+  //   this.estado=`se agregado una tarea
+  //    ${this.nombreTarea}
+  //   `
+  //   this.tareaAgregada=tru
+  //   this.tareas.push(this.nombreTarea)
+  //  }
 
-   setearNombreTarea(event: Event){
-     this.nombreTarea = (<HTMLInputElement>event.target).value
-   }
+  //  setearNombreTarea(event: Event){
+  //    this.nombreTarea = (<HTMLInputElement>event.target).value
+  //  }
 
   ngOnInit() {
   }
---
+++ src/app/modelos/Tarea.ts
@@ -0,0 +1,5 @@
+export interface iTarea{
+    titulo: string
+    fechaRegistro?: Date
+    
+}
